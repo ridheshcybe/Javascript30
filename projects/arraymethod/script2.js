2de81f5f40b70cf5fb11f1f92ee2b9d05eb81b8a
ridheshcybe <80441770+ridheshcybe@users.noreply.github.com>
2024-10-19 19:51:08 +0545

    day 7

---
+++ projects/arraymethod/script2.js
@@ -0,0 +1,16 @@
+const people = [{ name: 'ridhesh', age: 20 }, { name: 'jack', age: 19 }, { name: 'fortan', age: 22 }]
+const comments = [
+    { text: 'Love this!', id: 86286 },
+    { text: 'super good', id: 4741 },
+    { text: 'yes', id: 29402 },
+    { text: 'nice', id: 50880 }
+]
+
+const isAdult = people.some(e => e.age >= 19)
+const isEveryoneAdult = people.every(e => e.age >= 19);
+
+const findID = comments.find(e => e.id == 29402);
+const findIndex = comments.findIndex(e => e.id == 29402);
+
+console.table({ isAdult, isEveryoneAdult })
+console.table({ findID, find: comments[findIndex] })
